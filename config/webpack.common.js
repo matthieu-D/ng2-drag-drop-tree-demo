e95e56b8ed6326f2b31f21d99b655cce66513208
Matthieu Drula <matthieu.drula.hs@gmail.com>
2016-08-02 23:31:32 -0400

    handle loading for font awesome

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -154,6 +154,9 @@ module.exports = {
         loader: 'json-loader'
       },
 
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+
       /*
        * to string and css loader support for *.css files
        * Returns file content as string
